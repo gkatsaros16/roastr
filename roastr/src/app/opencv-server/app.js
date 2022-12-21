b9fb354c5c93081169226c9f6ba449bc0d047847
George <gkatsaros@harmonicdrive.net>
2022-12-21 08:31:20 -0500

    webcam first attempt

---
+++ roastr/src/app/opencv-server/app.js
@@ -0,0 +1,24 @@
+const cv = require('opencv4nodejs');
+const path = require('path');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const FPS = 60;
+const wCap = new cv.VideoCapture(0);
+// wCap.set(cv.CAP_PROP_FRAME_WIDTH)
+// wCap.set(cv.CAP_PROP_FRAME_HEIGHT)
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+setInterval(() => {
+    const frame = wCap.read();
+    const image = cv.imencode('.jpg', frame).toString('base64');
+    io.emit('image', image)
+}, 1000 / FPS);
+
+http.listen(4445, () => {
+console.log('Listening on port 4445');
+});
+
